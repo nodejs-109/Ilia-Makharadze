23addf71fc42d776e513169cafec51998a9c560d
ilia's hub <120248378+Ilia-Makharadze@users.noreply.github.com>
2025-04-08 18:28:45 +0400

    lesson8Homework

--- lesson3Homework/leapYear.js
+++
@@ -1,9 +0,0 @@
-let input=prompt("Enter year for which you want to know whether it is a leap year or not");
-if(input%400!=0 && input%100===0){
-    alert(input+" is not leap year")
-}
-else if(input%4===0){
-    alert(input+" is a leap year");
-}else{
-    alert(input+" is not leap year");
-}
---
+++ models/user.js
@@ -0,0 +1,16 @@
+// models/user.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+});
+
+module.exports = User;

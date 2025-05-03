8c0a4d2c9a47776427bae9c0c9c25238a3a11af9
ilia's hub <120248378+Ilia-Makharadze@users.noreply.github.com>
2025-05-03 19:40:52 +0400

    finalApp
    Full stack express js app

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('testdb', 'postgres', 'ilia', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;

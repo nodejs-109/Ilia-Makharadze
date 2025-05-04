2f77c745498af18dae935d0e079348b8331cf15e
ilia's hub <120248378+Ilia-Makharadze@users.noreply.github.com>
2025-05-04 12:06:49 +0400

    ♻️ Set up env vars and PostgreSQL config for Render deployment

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('registrationdb', 'postgres', 'ilia', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Render-ში საჭიროა
+        },
+    },
 });
 
 module.exports = sequelize;
